66ca0446d41cb3a64a8efded2918baa91e793093
chenpeleg <chenpeleg@gmail.com>
2022-09-27 16:42:17 +1000

    spelling

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -1,8 +1,8 @@
 describe('Only simple tests', () => {
-    it('assetrs that 8 is equalt to 8', () => {
+    it('Asserts that 8 is equal to 8', () => {
         expect(8).toBe(8);
     });
-    it('assetrs that 7 is not equalt to 8', () => {
+    it('Asserts that 7 is not equal to 8', () => {
         let seven = 7;
         expect(8 === seven).toBe(false);
     });
